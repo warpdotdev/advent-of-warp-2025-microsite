c2e7ef2dd2f52ecc364fc5135d510eb1bfb2cb2c
Ben Holmes <bholmesdev@gmail.com>
2025-12-10 13:10:46 -0500

    add first two days

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,7 +5,7 @@ import { z } from 'astro/zod';
 const days = defineCollection({
   loader: glob({ pattern: '**/*.md', base: './src/content/days' }),
   schema: z.object({
-    url: z.string().startsWith('/'),
+    url: z.string().url(),
   }),
 });
 
